f918345be16c0d75e7f34c22ea4d6eab082feb00
jinyongyun <102133961+jinyongyun@users.noreply.github.com>
2022-06-27 23:58:14 +0900

    Add files via upload

---
+++ LEDBOARD/LEDBOARD/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  LEDBOARD
+//
+//  Created by mac on 2022/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController, LEDBoardSettingDelegate {
+
+    @IBOutlet weak var contentsLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+       
+    }
+    
+  
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let settingViewController = segue.destination as? SettingViewController {
+            settingViewController.delegate = self
+            settingViewController.ledText = self.contentsLabel.text
+            settingViewController.textColor = self.contentsLabel.textColor
+            settingViewController.backgroundColor = self.view.backgroundColor ?? .black
+        }
+    }
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor) {
+        if let text = text {
+            self.contentsLabel.text = text
+            
+        }
+        self.contentsLabel.textColor = textColor
+        self.view.backgroundColor = backgroundColor
+    }
+}
+
